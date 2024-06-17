c79dff70c526f3587e9d7f7ff40c00f5ad803c45
thy <mailcuathy@gmail.com>
2024-06-18 04:20:52 +0700

    Update: Update main.jsx to use MUI theme and CssBaseline

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import CssBaseline from "@mui/material/CssBaseline";
+import { Experimental_CssVarsProvider as CssVarsProvider } from "@mui/material/styles";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+import ReactDOM from "react-dom/client";
+
+import App from "./App.jsx";
+import theme from "./theme.js";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <CssVarsProvider theme={theme} defaultMode="system">
+    <CssBaseline />
     <App />
-  </React.StrictMode>,
-)
+  </CssVarsProvider>
+);
